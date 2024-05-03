a3d103ae229f8ab834790cb1b69dfe0864d240a1
Dogukan Cifci <dogukancifcii5@gmail.com>
2024-05-03 16:53:27 +0300

    primitive type-wrapper type

---
+++ 01_Core_Java/src/main/java/com/dogukancifci/_01_WhatIsJava.java
@@ -0,0 +1,15 @@
+package com.dogukancifci;
+
+public class _01_WhatIsJava {
+    //psvm
+    public static void main(String[] args) {
+        //sout
+        System.out.println("Merhabalar Nasilsiniz");
+
+        // zero
+        // null
+        // create
+
+
+    }
+}
---
+++ 01_Core_Java/src/main/java/com/dogukancifci/_02_Datatypes.java
@@ -0,0 +1,31 @@
+package com.dogukancifci;
+
+public class _02_Datatypes {
+    public static void main(String[] args) {
+        //variables
+
+        // Tam sayılar
+        byte b1 =123;
+        System.out.println(b1);
+        short s1=12354;
+        int i1=1231235;
+        long l1=123451231;
+
+        // Virgüllü sayılar
+        float f2=1232.12F;
+        double d2=123213.21231;
+
+        char c1='A';
+        char c2='3';
+        char c3='b';
+        boolean b3=25>3;
+
+        //8 tane primative type vardir.
+        // primitive type - Wrapper type
+
+        int primitive=44;
+        // vermezsiniz=> primitive=null;
+        Integer objectInt=44;
+        objectInt=null;
+    }
+}
