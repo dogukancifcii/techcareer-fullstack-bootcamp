497040d8273b1e23a925fa985c485ded6ffca7af
Dogukan Cifci <dogukancifcii5@gmail.com>
2024-05-05 18:27:36 +0300

    pom.xml and .properties

--- 02_Spring_Boot/src/main/java/com/dogukancifci/Application.java
+++ 02_Spring_Boot/src/main/java/com/dogukancifci/Application.java
@@ -1,13 +1,45 @@
 package com.dogukancifci;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import java.util.TimeZone;
+
+
+// Spring Security: Şimdilik dahil etme ancak Spring security için gerekli kütüphaneleri dahil
+@SpringBootApplication(exclude = {
+		//SecurityAutoConfiguration.class,
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+}
+)
+// @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("IST"));
 	}
 
-}
+	public static void main(String[] args) {
+
+		// devtools active isActive
+		// System.setProperty("spring.devtools.restart.enabled","true");
+
+		// PORT Ayarlamak
+        /*
+        SpringApplication app = new SpringApplication(TurgutUniversitySpringAllInOneApplication.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", "8083"));
+        app.run(args);
+         */
+
+		// JOptional pane aktif etmek
+		System.setProperty("java.awt.headless", "false");
+
+		// Main
+		SpringApplication.run(Application.class, args);
+	}//end PSVM
+
+}//end class
