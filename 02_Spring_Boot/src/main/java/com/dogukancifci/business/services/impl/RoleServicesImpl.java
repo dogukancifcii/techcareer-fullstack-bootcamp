ae0be1e0c404126c71ad544603d788630d45d417
Dogukan Cifci <dogukancifcii5@gmail.com>
2024-05-06 12:30:21 +0300

    RoleServices

--- 02_Spring_Boot/src/main/java/com/dogukancifci/business/services/impl/RoleServicesImpl.java
+++ 02_Spring_Boot/src/main/java/com/dogukancifci/business/services/impl/RoleServicesImpl.java
@@ -3,44 +3,87 @@ package com.dogukancifci.business.services.impl;
 import com.dogukancifci.business.dto.RoleDto;
 import com.dogukancifci.business.services.IRoleService;
 import com.dogukancifci.data.entity.RoleEntity;
+import com.dogukancifci.data.repository.IRoleRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+//Lombok
+@RequiredArgsConstructor
+@Log4j2
+
+// Service: Asıl İş Yükünü sırtlayan
+@Service
+@Component("roleServicesImpl")  //Component => Spring'in bir parcasisin
 public class RoleServicesImpl implements IRoleService<RoleDto, RoleEntity> {
 
 
+    // Injection IRoleRepository (1.YOL => @Autowired)
+    /*
+    @Autowired
+    private IRoleRepository iRoleRepository;
+    */
+
+
+    // Injection IRoleRepository (2.YOL => Constructor Injection)
+    /*
+    private final IRoleRepository iRoleRepository;
+    @Autowired
+    public RoleServicesImpl(IRoleRepository iRoleRepository) {
+        this.iRoleRepository = iRoleRepository;
+    }
+    */
+
+    // 3. YOL (Lombok => Constructor Injection)
+    private final IRoleRepository iRoleRepository;
+    private final ModelMapper modelMapper;
+
+    ///////////////////////////////////////////////////////////////////////////////////////
+    //**** Model Mapper *****************************************************************//
+    // Model Mapper
     @Override
     public RoleDto entityToDto(RoleEntity roleEntity) {
-        return null;
+        return modelMapper.map(roleEntity,RoleDto.class);
     }
 
     @Override
     public RoleEntity dtoToEntity(RoleDto roleDto) {
-        return null;
+        return modelMapper.map(roleDto, RoleEntity.class);
     }
 
+    ///////////////////////////////////////////////////////////////////////////////////////
+    //**** CRUD*****************************************************************//
+    // Create
     @Override
     public RoleDto roleServiceCreate(RoleDto roleDto) {
         return null;
     }
 
+    // List
     @Override
     public List<RoleDto> roleServiceList(RoleDto roleDto) {
         return null;
     }
 
+    // Find
     @Override
     public RoleDto roleServiceFindById(Long id) {
         return null;
     }
 
+    // Update
     @Override
     public RoleDto roleServiceUpdateById(Long id, RoleDto roleDto) {
         return null;
     }
 
+    // Delete
     @Override
     public RoleDto roleServiceDeleteById(Long id) {
         return null;
     }
-}
+}// End RoleServicesImpl
