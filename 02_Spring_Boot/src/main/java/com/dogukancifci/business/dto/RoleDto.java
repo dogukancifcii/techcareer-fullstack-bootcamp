3c9dd24aebbf338ceea7fda3d3ccbafbf7a39463
Dogukan Cifci <dogukancifcii5@gmail.com>
2024-05-06 10:08:26 +0300

    Role Dto

--- 02_Spring_Boot/src/main/java/com/dogukancifci/business/dto/RoleDto.java
+++ 02_Spring_Boot/src/main/java/com/dogukancifci/business/dto/RoleDto.java
@@ -1,5 +1,6 @@
 package com.dogukancifci.business.dto;
 
+import com.dogukancifci.role.ERole;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
 import lombok.extern.log4j.Log4j2;
@@ -17,13 +18,19 @@ import java.util.Date;
 
 //RoleDto
 public class RoleDto implements Serializable {
+
+    //SERILESTIRME
+    public static final Long serialVersionUID = 1L;
+
     //Role ID
     private  Long roleId;
 
     //Role Name
     //Validation oto control
     @NotEmpty(message = "{role.name.validation.constraints.NotNull.message}")
-    private String roleName;
+    @Builder.Default
+    //Annotation kullanmalisin cunku database ayni role adinda olmamasi gerekiyor(unique)
+    private String roleName= ERole.USER.toString();
 
     //System Created Date
     private Date systemCreatedDate;
