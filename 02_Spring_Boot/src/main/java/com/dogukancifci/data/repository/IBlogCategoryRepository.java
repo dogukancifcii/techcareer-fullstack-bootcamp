9bcdaa102783a7ac3662bfd290372a8a49f5380c
Dogukan Cifci <dogukancifcii5@gmail.com>
2024-05-13 09:22:06 +0300

    Category(1)-Blog(N)

---
+++ 02_Spring_Boot/src/main/java/com/dogukancifci/data/repository/IBlogCategoryRepository.java
@@ -0,0 +1,18 @@
+package com.dogukancifci.data.repository;
+
+import com.dogukancifci.data.entity.BlogCategoryEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+// CrudRepository<RegisterEntity,Long>
+// JpaRepository<RegisterEntity,Long>
+// PagingAndSortingRepository<RegisterEntity,Long>
+
+@Repository
+public interface IBlogCategoryRepository extends CrudRepository<BlogCategoryEntity,Long> {
+
+    // Delivered Query (Kendi sorgumu yazdım)
+    Optional<BlogCategoryEntity> findByCategoryName(String categoryName);
+}
