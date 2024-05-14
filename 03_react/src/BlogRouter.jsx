91131ff2746cbaea7026841e557fcd12c41bab47
Dogukan Cifci <dogukancifcii5@gmail.com>
2024-05-14 09:16:58 +0300

    CRUD List

--- 03_react/src/BlogRouter.jsx
+++ 03_react/src/BlogRouter.jsx
@@ -6,7 +6,10 @@ import FooterComponent from "./components/FooterComponent";
 import HeaderComponent from "./components/HeaderComponent";
 import MainComponent from "./components/MainComponent";
 import React from "react";
-
+import BlogCategoryList from "./components/blog_category/BlogCategoryList";
+import BlogCategoryCreate from "./components/blog_category/BlogCategoryCreate";
+import BlogCategoryView from "./components/blog_category/BlogCategoryView";
+import BlogCategoryUpdate from "./components/blog_category/BlogCategoryUpdate";
 // CLASS COMPONENT BlogRouter
 function BlogRouter() {
   // RETURN
@@ -19,10 +22,23 @@ function BlogRouter() {
           <Route path={"/"} element={<MainComponent />} />
           <Route path={"/index"} element={<MainComponent />} />
 
+          {/* Blog Categories */}
+          <Route path={"/blog/category/list"} element={<BlogCategoryList />} />
+          <Route
+            path={"/blog/category/create"}
+            element={<BlogCategoryCreate />}
+          />
+          <Route
+            path={"/blog/category/view/:id"}
+            element={<BlogCategoryView />}
+          />
+          <Route
+            path={"/blog/category/update/:id"}
+            element={<BlogCategoryUpdate />}
+          />
+
           <Route path={"*"} element={<Navigate to={"/"} />} />
-          {/* <Route path="/about" element={<About />} />
-                            <Route path="/contact" element={<Contact />} />
-                            <Route path="/blog" element={<Blog />} />
+          {/* 
                             <Route path="/blog/:slug" element={<BlogDetail />} />
                             <Route path="*" element={<NotFound />} /> */}
         </Routes>
---
+++ 03_react/src/components/blog_category/BlogCategoryCreate.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function BlogCategoryCreate() {
+  return <div>BlogCategoryCreate</div>;
+}
